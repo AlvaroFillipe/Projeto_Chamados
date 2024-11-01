27e0efc7692507c3620ec06ec0d33c6d36663954
Alvaro Fillipe <97744732+AlvaroFillipe@users.noreply.github.com>
2024-11-01 18:45:30 -0300

    .

--- documentação/BKP documentacao/banco de dados.sql
+++ documentação/BKP documentacao/banco de dados.sql
@@ -5,7 +5,8 @@ CREATE DATABASE db_projeto_chamados;
 
 CREATE TABLE tb_departamentos(
     pk_id_departamento int not null auto_increment primary key,   
-    departamento varchar(30) not null
+    departamento varchar(30) not null,
+    situacao_departamento int not null
 );
 
 CREATE TABLE tb_usuarios(   
@@ -14,6 +15,7 @@ CREATE TABLE tb_usuarios(
     usuario varchar(150) not null,
     tipo_usuario int not null,
     fk_id_departamento int not null,
+    situacao_usuario int not null,
     senha varchar(32) not null,   
 
      FOREIGN KEY(fk_id_departamento)REFERENCES tb_departamentos(pk_id_departamento)
@@ -26,8 +28,9 @@ CREATE TABLE tb_chamados(
     fk_id_departamento int not null,
     chamado text not null,
     status_chamado int not null,
-    solucao_chamado text,
+    solucao_chamado text not null,
     data_chamado DATETIME DEFAULT CURRENT_TIMESTAMP,
+    situacao_chamado int not null,
 
     FOREIGN KEY(fk_id_usuario)REFERENCES tb_usuarios(pk_id_usuario),
     FOREIGN KEY(fk_id_departamento)REFERENCES tb_departamentos(pk_id_departamento)
@@ -37,21 +40,23 @@ CREATE TABLE tb_chamados(
 INSERT INTO 
     tb_departamentos
         (
-        departamento
+        departamento,
+        situacao_departamento
         )
 VALUES
-        ("dep_piloto");.
+        ("dep_piloto",1);.
 INSERT INTO 
     tb_usuarios
         (
         email, 
         usuario, 
         tipo_usuario,
-        fk_id_departamento,         
+        fk_id_departamento,
+        situacao_usuario         
         senha
         )
 VALUES
-        ("alvarofillipe6@gmail.com","alvaro",1,1,"123");
+        ("alvarofillipe6@gmail.com","alvaro",1,1,1,"123");
 
 
 
